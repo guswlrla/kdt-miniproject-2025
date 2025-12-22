e3f5381f2926869cc85cc5d98849c12f413e8c27
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-12-22 17:53:02 +0900

    add map polygon

--- ERInfo_FE/src/app/kakaoMap.tsx
+++ ERInfo_FE/src/app/kakaoMap.tsx
@@ -1,11 +1,39 @@
 'use client'
-import { Map, MapMarker } from "react-kakao-maps-sdk"
+import { useEffect, useState } from "react"
+import { Map, MapMarker, Polygon } from "react-kakao-maps-sdk"
 
 function BasicMap() {
+  const busan_bounds = {
+    sw: { lat: 34.88, lng: 128.83 },
+    ne: { lat: 35.39, lng: 129.35 }
+  }
+
+  function isOutOfBusan(lat: number, lng: number) {
+    return (
+      lat < busan_bounds.sw.lat ||
+      lat > busan_bounds.ne.lat ||
+      lng < busan_bounds.sw.lng ||
+      lng > busan_bounds.ne.lng
+    )
+  }
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100%" }} className="rounded-xl shadow-md border border-gray-200">
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Team.현지현지</div>
+    <Map center={{ lat: 35.1796, lng: 129.0756 }} minLevel={9} style={{ width: "100%", height: "100%" }}
+         onDragEnd={map => {
+           const center = map.getCenter()
+           const lat = center.getLat()
+           const lng = center.getLng()
+
+           if(isOutOfBusan(lat, lng)) {
+             map.setCenter(new kakao.maps.LatLng(35.1796, 129.0756))
+           }}}
+         className="rounded-xl shadow-md border border-gray-200">
+      <MapMarker position={{ lat: 35.1796, lng: 129.0756 }}>
+        <div style={{ color: "#000" }}>현지현지</div>
       </MapMarker>
     </Map>
   )
